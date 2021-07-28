90275f8c992976d0e73b09fd1a64cd70b6fbcc79
preetip26 <preeti.poddar@somaiya.edu>
2021-07-28 09:21:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryingVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
